0653b11665dc4e2c3e497a243912456fd1b80b9d
Paul Ross <apaulross@gmail.com>
2021-05-24 17:16:47 +0100

    Add test_perf_py_tuple_to_vector().

--- src/test_performance.cpp
+++ src/test_performance.cpp
@@ -17,6 +17,15 @@ int test_perf_vector_to_py_tuple(TestResultS &test_results, const std::string &t
     return result;
 }
 
+template<typename T, PyObject *(*ConvertCppToPy)(const T &), T (*ConvertPyToCpp)(PyObject *)>
+int test_perf_py_tuple_to_vector(TestResultS &test_results, const std::string &type) {
+    int result = 0;
+    for (size_t size = 2; size < 1 << 24; size *= 2) {
+        result |= test_py_tuple_to_vector<T, ConvertCppToPy, ConvertPyToCpp>(test_results, type, size);
+    }
+    return result;
+}
+
 int test_perf_vector_string_to_py_tuple(TestResultS &test_results) {
     int result = 0;
     for (size_t str_len = 1; str_len <= 512; str_len *=2) {
@@ -38,14 +47,31 @@ int test_perf_py_tuple_string_to_vector(TestResultS &test_results) {
 }
 
 void test_performance_all(TestResultS &test_results) {
-    test_perf_vector_to_py_tuple<bool, &Python_Cpp_Homogeneous_Containers::py_bool_as_bool>(test_results,
-                                                                                                 "<bool>");
-    test_perf_vector_to_py_tuple<long, &Python_Cpp_Homogeneous_Containers::py_long_as_long>(test_results,
-                                                                                                 "<long>");
-    test_perf_vector_to_py_tuple<double, &Python_Cpp_Homogeneous_Containers::py_float_as_double>(test_results,
-                                                                                                 "<double>");
+    test_perf_vector_to_py_tuple<
+            bool, &Python_Cpp_Homogeneous_Containers::py_bool_as_bool
+    >(test_results, "<bool>");
+    test_perf_vector_to_py_tuple<
+            long, &Python_Cpp_Homogeneous_Containers::py_long_as_long
+    >(test_results, "<long>");
+    test_perf_vector_to_py_tuple<
+            double,
+            &Python_Cpp_Homogeneous_Containers::py_float_as_double
+    >(test_results, "<double>");
+    test_perf_py_tuple_to_vector<
+            bool,
+            &Python_Cpp_Homogeneous_Containers::py_bool_from_bool,
+            &Python_Cpp_Homogeneous_Containers::py_bool_as_bool
+    >(test_results, "<bool>");
+    test_perf_py_tuple_to_vector<
+            long,
+            &Python_Cpp_Homogeneous_Containers::py_long_from_long,
+            &Python_Cpp_Homogeneous_Containers::py_long_as_long
+    >(test_results, "<long>");
+    test_perf_py_tuple_to_vector<
+            double,
+            &Python_Cpp_Homogeneous_Containers::py_float_from_double,
+            &Python_Cpp_Homogeneous_Containers::py_float_as_double
+    >(test_results, "<double>");
     test_perf_vector_string_to_py_tuple(test_results);
     test_perf_py_tuple_string_to_vector(test_results);
 }
-
-
