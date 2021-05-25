38ae69435dcef40eeb012b4021041cab5f60b45e
Paul Ross <apaulross@gmail.com>
2021-05-25 09:32:06 +0100

    Memory investigation.

--- main.cpp
+++ main.cpp
@@ -6,15 +6,28 @@
 #include "test_performance.h"
 
 
+void test_all() {
+    TestResultS test_results;
+
+//    test_functional_all(test_results);
+    test_performance_all(test_results);
+
+    std::cout << test_results << std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     Py_Initialize();
 
-    TestResultS test_results;
+    std::cout << "Enter to start... ";
+    std::cout.flush();
+    system("read");
 
-    test_functional_all(test_results);
-    test_performance_all(test_results);
+    test_all();
 
-    std::cout << test_results << std::endl;
+    std::cout << "Enter to finish... ";
+    std::cout.flush();
+    system("read");
+    std::cout << "Bye, bye!" << std::endl;
     return 0;
 }
--- src/test_performance.cpp
+++ src/test_performance.cpp
@@ -28,7 +28,7 @@ int test_perf_py_tuple_to_vector(TestResultS &test_results, const std::string &t
 
 int test_perf_vector_string_to_py_tuple(TestResultS &test_results) {
     int result = 0;
-    for (size_t str_len = 1; str_len <= 512; str_len *=2) {
+    for (size_t str_len = 1; str_len <= 4096; str_len *= 4) {
         for (size_t size = 2; size < 1 << 21; size *= 2) {
             result |= test_vector_string_to_py_tuple(test_results, size, str_len);
         }
@@ -38,7 +38,7 @@ int test_perf_vector_string_to_py_tuple(TestResultS &test_results) {
 
 int test_perf_py_tuple_string_to_vector(TestResultS &test_results) {
     int result = 0;
-    for (size_t str_len = 1; str_len <= 512; str_len *=2) {
+    for (size_t str_len = 1; str_len <= 4096; str_len *= 4) {
         for (size_t size = 2; size < 1 << 21; size *= 2) {
             result |= test_py_tuple_string_to_vector(test_results, size, str_len);
         }
@@ -75,44 +75,46 @@ int test_perf_py_dict_to_cpp_std_unordered_map(TestResultS &test_results, const 
 }
 
 void test_performance_all(TestResultS &test_results) {
-    test_perf_vector_to_py_tuple<
-            bool, &Python_Cpp_Homogeneous_Containers::py_bool_as_bool
-    >(test_results, "<bool>");
-    test_perf_vector_to_py_tuple<
-            long, &Python_Cpp_Homogeneous_Containers::py_long_as_long
-    >(test_results, "<long>");
-    test_perf_vector_to_py_tuple<
-            double,
-            &Python_Cpp_Homogeneous_Containers::py_float_as_double
-    >(test_results, "<double>");
-    test_perf_py_tuple_to_vector<
-            bool,
-            &Python_Cpp_Homogeneous_Containers::py_bool_from_bool,
-            &Python_Cpp_Homogeneous_Containers::py_bool_as_bool
-    >(test_results, "<bool>");
-    test_perf_py_tuple_to_vector<
-            long,
-            &Python_Cpp_Homogeneous_Containers::py_long_from_long,
-            &Python_Cpp_Homogeneous_Containers::py_long_as_long
-    >(test_results, "<long>");
-    test_perf_py_tuple_to_vector<
-            double,
-            &Python_Cpp_Homogeneous_Containers::py_float_from_double,
-            &Python_Cpp_Homogeneous_Containers::py_float_as_double
-    >(test_results, "<double>");
-    test_perf_vector_string_to_py_tuple(test_results);
-    test_perf_py_tuple_string_to_vector(test_results);
+//    test_perf_vector_to_py_tuple<
+//            bool, &Python_Cpp_Homogeneous_Containers::py_bool_as_bool
+//    >(test_results, "<bool>");
+//    test_perf_vector_to_py_tuple<
+//            long, &Python_Cpp_Homogeneous_Containers::py_long_as_long
+//    >(test_results, "<long>");
 
-    test_perf_cpp_std_unordered_map_to_py_dict<
-            double,
+//    test_perf_vector_to_py_tuple<
+//            double,
+//            &Python_Cpp_Homogeneous_Containers::py_float_as_double
+//    >(test_results, "<double>");
+
+    //    test_perf_py_tuple_to_vector<
+//            bool,
+//            &Python_Cpp_Homogeneous_Containers::py_bool_from_bool,
+//            &Python_Cpp_Homogeneous_Containers::py_bool_as_bool
+//    >(test_results, "<bool>");
+//    test_perf_py_tuple_to_vector<
+//            long,
+//            &Python_Cpp_Homogeneous_Containers::py_long_from_long,
+//            &Python_Cpp_Homogeneous_Containers::py_long_as_long
+//    >(test_results, "<long>");
+    test_perf_py_tuple_to_vector<
             double,
             &Python_Cpp_Homogeneous_Containers::py_float_from_double,
-            &Python_Cpp_Homogeneous_Containers::py_float_from_double
-            >(test_results, "<double>");
-    test_perf_py_dict_to_cpp_std_unordered_map<
-            double,
-            double,
-            &Python_Cpp_Homogeneous_Containers::py_float_from_double,
-            &Python_Cpp_Homogeneous_Containers::py_float_from_double
-            >(test_results, "<double>");
+            &Python_Cpp_Homogeneous_Containers::py_float_as_double
+    >(test_results, "<double>");
+//    test_perf_vector_string_to_py_tuple(test_results);
+//    test_perf_py_tuple_string_to_vector(test_results);
+//
+//    test_perf_cpp_std_unordered_map_to_py_dict<
+//            double,
+//            double,
+//            &Python_Cpp_Homogeneous_Containers::py_float_from_double,
+//            &Python_Cpp_Homogeneous_Containers::py_float_from_double
+//            >(test_results, "<double>");
+//    test_perf_py_dict_to_cpp_std_unordered_map<
+//            double,
+//            double,
+//            &Python_Cpp_Homogeneous_Containers::py_float_from_double,
+//            &Python_Cpp_Homogeneous_Containers::py_float_from_double
+//            >(test_results, "<double>");
 }
