462523238f246ecddc3b2c753406eeac8faf5960
Paul Ross <apaulross@gmail.com>
2021-10-04 12:17:27 +0100

    WIP: Tidy main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -36,11 +36,7 @@ void free_new(void *ctx, void *ptr) {
     free(ptr);
 }
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    ExecClock exec_clock;
-    Py_Initialize();
-
+void explore_memory_allocation_substitution() {
     PyMemAllocatorEx allocator_raw;
     PyMem_GetAllocator(PYMEM_DOMAIN_RAW, &allocator_raw);
     PyMemAllocatorEx allocator_mem;
@@ -58,28 +54,27 @@ int main() {
     PyObject *foo = PyBytes_FromString("Hello world.");
     Py_DECREF(foo);
 
+    std::cout << "Count of unique strings: " << count_of_unique_string() << std::endl;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    ExecClock exec_clock;
+    Py_Initialize();
+
     RSSSnapshot rss_overall("main.cpp");
 
-//    std::cout << "Enter to start... ";
-//    std::cout.flush();
-//    system("read");
+//    test_all();
 
-    test_all();
+    TestResultS test_results;
+    test_functional_all(test_results);
+    test_memory_all(test_results);
 
-    std::cout << "Count of unique strings: " << count_of_unique_string() << std::endl;
-
-//    std::cout << "Enter to Py_FinalizeEx... ";
-//    std::cout.flush();
-//    system("read");
+    std::cout << test_results << std::endl;
 
     Py_FinalizeEx();
 
-//    std::cout << "Enter to finish... ";
-//    std::cout.flush();
-//    system("read");
-
     std::cout << "====" << rss_overall << std::endl;
-
     std::cout << "Total execution time: " << exec_clock.seconds() << " (s)" << std::endl;
     std::cout << "Bye, bye!" << std::endl;
     return 0;
