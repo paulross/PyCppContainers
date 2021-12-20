304a8c3e4386e12c21131c25c264765b6858d1f5
Paul Ross <apaulross@gmail.com>
2021-12-20 19:23:23 +0000

    WIP Add round trip set tests. Mark performance tests as slow

--- tests/unit/test_perf_cPyCppContainers.py
+++ tests/unit/test_perf_cPyCppContainers.py
@@ -1,4 +1,9 @@
-import pprint
+"""
+This is a fairly exhaustive set of tests that take a long time to run.
+
+Run this with:
+$ pytest -vs --runslow tests/unit/test_perf_cPyCppContainers.py
+"""
 import random
 import statistics
 import time
@@ -11,7 +16,8 @@ import cPyCppContainers
 
 SIZE_DOUBLING = tuple(2 ** v for v in range(1, 20 + 1))
 # SIZE_DOUBLING = tuple(2 ** v for v in range(1, 16 + 1))
-SIZE_DOUBLING_BYTE_LENGTH = (8, 64, 512, 4096)
+# SIZE_DOUBLING_BYTE_LENGTH = (8, 64, 512, 4096)
+SIZE_DOUBLING_BYTE_LENGTH = (2, 16, 128, 1024)
 # SIZE_DOUBLING_BYTE_LENGTH = (8, 64)
 REPEAT = 5
 
@@ -64,6 +70,7 @@ class TimedResults:
         return len(self.times)
 
 
+@pytest.mark.slow
 def test_new_list_bool():
     results = []
     proc = psutil.Process()
@@ -85,9 +92,9 @@ def test_new_list_bool():
     print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
     for s, t in results:
         print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
-    assert 0
 
 
+@pytest.mark.slow
 def test_new_list_int():
     results = []
     proc = psutil.Process()
@@ -109,9 +116,9 @@ def test_new_list_int():
     print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
     for s, t in results:
         print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
-    assert 0
 
 
+@pytest.mark.slow
 def test_new_list_float():
     results = []
     proc = psutil.Process()
@@ -136,7 +143,6 @@ def test_new_list_float():
     print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
     for s, t in results:
         print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
-    assert 0
 
 
 def random_bytes(byte_values: typing.List[int], length: int) -> bytes:
@@ -163,9 +169,6 @@ def _test_new_list_bytes():
         # 1Gb limit is 2**30
         max_size = 2 ** 24 // byte_length
         for size in SIZE_DOUBLING:
-            if size > max_size:
-                print(f'Breaking as max size {max_size} reached memory {max_size * byte_length:,d}.')
-                break
             # original = [BYTES_LISTS[byte_length] for _i in range(size)]
             original_bytes = b' ' * byte_length
             original = [original_bytes for _i in range(size)]
@@ -187,12 +190,95 @@ def _test_new_list_bytes():
     print(f'RSS was {rss:,d} now {rss_new:,d} diff: {rss_new - rss:,d}')
 
 
+@pytest.mark.slow
 def test_new_list_bytes():
     for i in range(1):
         _test_new_list_bytes()
-    assert 0
 
 
+@pytest.mark.slow
+def test_new_set_int():
+    results = []
+    proc = psutil.Process()
+    rss = proc.memory_info().rss
+    for size in SIZE_DOUBLING:
+        original = {i for i in range(size)}
+        timer = TimedResults()
+        for _r in range(REPEAT):
+            time_start = time.perf_counter()
+            cPyCppContainers.new_set_int(original)
+            time_exec = time.perf_counter() - time_start
+            timer.add(time_exec)
+        results.append((size, timer))
+    # pprint.pprint(results)
+    print()
+    rss_new = proc.memory_info().rss
+    print('test_new_set_int()')
+    print(f'RSS was {rss:,d} now {rss_new:,d} diff: {rss_new - rss:,d}')
+    print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
+    for s, t in results:
+        print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
+
+
+@pytest.mark.slow
+def test_new_set_float():
+    results = []
+    proc = psutil.Process()
+    rss = proc.memory_info().rss
+    # for size in tuple(2 ** v for v in range(1, 16 + 1)):
+    # for size in tuple(2 ** v for v in range(10, 11 + 1)):
+    for size in SIZE_DOUBLING:
+        original = {float(i) for i in range(size)}
+        timer = TimedResults()
+        # for _r in range(REPEAT):
+        for _r in range(1):
+            time_start = time.perf_counter()
+            cPyCppContainers.new_set_float(original)
+            time_exec = time.perf_counter() - time_start
+            timer.add(time_exec)
+        results.append((size, timer))
+    # pprint.pprint(results)
+    print()
+    rss_new = proc.memory_info().rss
+    print('test_new_set_float()')
+    print(f'RSS was {rss:,d} now {rss_new:,d} diff: {rss_new - rss:,d}')
+    print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
+    for s, t in results:
+        print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
+
+
+@pytest.mark.slow
+def test_new_set_bytes():
+    random_bytes = [random.randint(0, 255) for _i in range(max(SIZE_DOUBLING_BYTE_LENGTH))]
+    proc = psutil.Process()
+    for byte_length in SIZE_DOUBLING_BYTE_LENGTH:
+        results = []
+        rss = proc.memory_info().rss
+        for size in SIZE_DOUBLING:
+            original = set()
+            for i in range(size):
+                random.shuffle(random_bytes)
+                k = bytes(random_bytes[:byte_length])
+                original.add(k)
+            # print(f'TRACE dict len {len(original)}')
+            timer = TimedResults()
+            for _r in range(REPEAT):
+                time_start = time.perf_counter()
+                cPyCppContainers.new_set_bytes(original)
+                time_exec = time.perf_counter() - time_start
+                timer.add(time_exec)
+            results.append((size, timer))
+        # pprint.pprint(results)
+        print()
+        rss_new = proc.memory_info().rss
+        print(f'RSS was {rss:,d} now {rss_new:,d} diff: {rss_new - rss:,d}')
+        print(f'test_new_set_bytes() Byte length {byte_length}')
+        print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
+        for s, t in results:
+            print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
+
+
+@pytest.mark.slow
 def test_new_dict_int_int():
     results = []
     proc = psutil.Process()
@@ -214,16 +300,16 @@ def test_new_dict_int_int():
     print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
     for s, t in results:
         print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
-    assert 0
 
 
+@pytest.mark.slow
 def test_new_dict_float_float():
     results = []
     proc = psutil.Process()
     rss = proc.memory_info().rss
-    # for size in SIZE_DOUBLING:
     # for size in tuple(2 ** v for v in range(1, 16 + 1)):
-    for size in tuple(2 ** v for v in range(10, 11 + 1)):
+    # for size in tuple(2 ** v for v in range(10, 11 + 1)):
+    for size in SIZE_DOUBLING:
         original = {float(i): float(i) for i in range(size)}
         timer = TimedResults()
         # for _r in range(REPEAT):
@@ -241,57 +327,9 @@ def test_new_dict_float_float():
     print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
     for s, t in results:
         print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
-    assert 0
 
 
-def test_new_dict_debug_int_float():
-    proc = psutil.Process()
-    rss = proc.memory_info().rss
-    results = []
-    # for size in SIZE_DOUBLING:
-    # for size in tuple(2 ** v for v in range(1, 16 + 1)):
-    for size in tuple(2 ** v for v in range(10, 12 + 1)):
-        original = {i: i for i in range(size)}
-        timer = TimedResults()
-        # for _r in range(REPEAT):
-        for _r in range(5):
-            time_start = time.perf_counter()
-            cPyCppContainers.new_dict_debug_int_int(original)
-            time_exec = time.perf_counter() - time_start
-            timer.add(time_exec)
-        results.append((size, timer))
-    # pprint.pprint(results)
-    print()
-    rss_new = proc.memory_info().rss
-    print('test_new_dict_debug_float_int() with cPyCppContainers.new_dict_debug_int_int()')
-    print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
-    for s, t in results:
-        print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
-    print(f'RSS was {rss:,d} now {rss_new:,d} diff: {rss_new - rss:,d}')
-    results = []
-    # for size in SIZE_DOUBLING:
-    # for size in tuple(2 ** v for v in range(1, 16 + 1)):
-    for size in tuple(2 ** v for v in range(10, 12 + 1)):
-        original = {float(i): float(i) for i in range(size)}
-        timer = TimedResults()
-        # for _r in range(REPEAT):
-        for _r in range(5):
-            time_start = time.perf_counter()
-            cPyCppContainers.new_dict_debug_float_float(original)
-            time_exec = time.perf_counter() - time_start
-            timer.add(time_exec)
-        results.append((size, timer))
-    # pprint.pprint(results)
-    print()
-    rss_new = proc.memory_info().rss
-    print('test_new_dict_debug_float_int() with cPyCppContainers.new_dict_debug_float_float()')
-    print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
-    for s, t in results:
-        print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
-    print(f'RSS was {rss:,d} now {rss_new:,d} diff: {rss_new - rss:,d}')
-    assert 0
-
-
+@pytest.mark.slow
 def test_new_dict_bytes_bytes():
     random_bytes = [random.randint(0, 255) for _i in range(max(SIZE_DOUBLING_BYTE_LENGTH))]
     proc = psutil.Process()
@@ -312,8 +350,6 @@ def test_new_dict_bytes_bytes():
                 time_exec = time.perf_counter() - time_start
                 timer.add(time_exec)
             results.append((size, timer))
-            if size >= 1024 * 128:
-                break
         # pprint.pprint(results)
         print()
         rss_new = proc.memory_info().rss
@@ -322,4 +358,3 @@ def test_new_dict_bytes_bytes():
         print(f'{"Size":<8s} {results[0][1].str_header():s} {"Min/Size e9":>12s}')
         for s, t in results:
             print(f'{s:<8d} {t} {1e9 * t.min() / s:12.1f}')
-    assert 0
