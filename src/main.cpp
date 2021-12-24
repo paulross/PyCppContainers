681c29922220a660d592a0fe5c38d4b773445ed9
Paul Ross <apaulross@gmail.com>
2021-12-24 10:00:21 +0000

    WIP Move PyMemAllocator.h/.cpp to pymemtrace project develop branch.

--- src/main.cpp
+++ src/main.cpp
@@ -18,46 +18,6 @@ void test_all() {
     std::cout << test_results << std::endl;
 }
 
-void *malloc_new(void *ctx, size_t size) {
-    void *ret = malloc(size);
-    return ret;
-}
-
-void *calloc_new(void *ctx, size_t nelem, size_t size) {
-    void *ret = calloc(nelem, size);
-    return ret;
-}
-
-void *realloc_new(void *ctx, void *ptr, size_t new_size) {
-    void *ret = realloc(ptr, new_size);
-    return ret;
-}
-
-void free_new(void *ctx, void *ptr) {
-    free(ptr);
-}
-
-void explore_memory_allocation_substitution() {
-    PyMemAllocatorEx allocator_raw;
-    PyMem_GetAllocator(PYMEM_DOMAIN_RAW, &allocator_raw);
-    PyMemAllocatorEx allocator_mem;
-    PyMem_GetAllocator(PYMEM_DOMAIN_MEM, &allocator_mem);
-    PyMemAllocatorEx allocator_obj;
-    PyMem_GetAllocator(PYMEM_DOMAIN_OBJ, &allocator_obj);
-
-    PyMemAllocatorEx allocator_mem_new{NULL, &malloc_new, &calloc_new, &realloc_new, &free_new};
-//    PyMem_SetAllocator(PYMEM_DOMAIN_MEM, &allocator_mem_new);
-//    PyMem_SetAllocator(PYMEM_DOMAIN_OBJ, &allocator_mem_new);
-
-    PyMem_GetAllocator(PYMEM_DOMAIN_MEM, &allocator_mem);
-    PyMem_GetAllocator(PYMEM_DOMAIN_OBJ, &allocator_obj);
-
-    PyObject *foo = PyBytes_FromString("Hello world.");
-    Py_DECREF(foo);
-
-    std::cout << "Count of unique strings: " << count_of_unique_string() << std::endl;
-}
-
 void explore_hash_reserve() {
     std::cout << "std::unordered_map<long, long>" << std::endl;
     for (int i = 1024; i < 1024 * 1024 * 2; i *= 2) {
